8626d4551910e5168bcf28a860e86109568463df
Guillaume Sammut <guillaume.sammut@myje.fr>
2019-09-10 15:32:18 +0200

    Initial commit from gatsby
    src: https://github.com/gatsbyjs/gatsby-starter-hello-world.git

---
+++ src/pages/index.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+export default () => (
+  <div>
+    <h1>Hello world!</h1>
+    <p>
+      Odio donec tincidunt torquent etiam natoque non mollis id bibendum fames
+    </p>
+  </div>
+)
